b01a7f38e1874a64f49ccdd732ecaf842ad00827
OmerYes <48590724+OmerYes@users.noreply.github.com>
2020-10-23 16:46:05 +0300

    -repository değişikliği yapıldı

---
+++ Credati/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Credati.Controllers
+{
+    [RoutePrefix("Credati")]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Route("~/")]
+        [Route("anasayfa")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("iletisim")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        [Route("hakkimizda")]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("yazilim")]
+        public ActionResult Software()
+        {
+            return View();
+        }
+        [Route("outsourcing")]
+        public ActionResult Outsourcing()
+        {
+            return View();
+        }
+        [Route("bt-cozümleri")]
+        public ActionResult It()
+        {
+            return View();
+        }
+        [Route("danismanlik")]
+        public ActionResult Constult()
+        {
+            return View();
+        }
+
+        [Route("iletisim")]
+        [HttpPost]
+        public ActionResult Contact(string name)
+        {
+            return View();
+        }
+    }
+}
